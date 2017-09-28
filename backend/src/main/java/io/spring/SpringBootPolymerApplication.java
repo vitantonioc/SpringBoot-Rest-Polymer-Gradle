2fef24d768caa274832c5e0e7ca952d4ebbfea02
Vitantonio Castronuovo <me@vitantonio.eu>
2017-09-28 19:18:18 +0200

    Add files via upload

---
+++ backend/src/main/java/io/spring/SpringBootPolymerApplication.java
@@ -0,0 +1,12 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPolymerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPolymerApplication.class, args);
+	}
+}
